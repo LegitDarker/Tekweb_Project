8c2c49fde56924f2258c87128f5ccc2bf16d20f1
LegitDarker <d4niel.ap@gmail.com>
2022-12-08 17:48:19 +0700

    Login & Register page selesai 100%

--- login_page.php
+++ login_page.php
@@ -3,6 +3,7 @@
 <head>
     <title>Login</title>
     <link rel="stylesheet" type="text/css" href="bootstrap-5.2.0-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="webcss.css">
     <style>
         body{font: 14px sans-serif; }
         .wrapper{width:360px; padding: 20px; margin:0 auto}
@@ -10,7 +11,11 @@
 </head>
 <body>
     <div class="wrapper">
-        <form action="loginprocess.php" method="post">
+        <video autoplay loop muted plays-inline class="back-video">
+            <source src="Image files/sign.mp4" type="video/mp4">
+        </video>
+
+        <form action="loginprocess.php" method="post" class="signin-box" style="margin-left: -120px; margin-top: 100px; color: white; background: black;">
             <h2>Login</h2>
             <p class="hint-text">Enter login details</p>
             <div class="mb-3">
@@ -29,7 +34,7 @@
             </div>
 
             <div class="form-group">
-                <button type="submit" name="save" class="btn btn-success btn-lg btn-block">
+                <button type="submit" name="save" class="btn btn-lg btn-block" style="background: white; color: black;">
                     Login
                 </button>
             </div>
--- register_page.php
+++ register_page.php
@@ -3,6 +3,7 @@
 <head>
     <title>Login</title>
     <link rel="stylesheet" type="text/css" href="bootstrap-5.2.0-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="webcss.css">
     <style>
         body{font: 14px sans-serif; }
         .wrapper{width:360px; padding: 20px; margin:0 auto}
@@ -11,9 +12,13 @@
 
 <body>
     <div class="wrapper">
-        <h2>Sign up</h2>
-        <p>Please fill this form to create an account.</p>
-        <form action="register_account.php" method="post">
+        <video autoplay loop muted plays-inline class="back-video">
+            <source src="Image files/sign.mp4" type="video/mp4">
+        </video>
+
+        <form action="register_account.php" method="post" class="signin-box" style="margin-left: -120px; margin-top: 100px; color: white; background: black;">
+            <h2>Sign up</h2>
+            <p>Please fill this form to create an account.</p>
 
             <div class="mb-3">
                 <label class="form-label">Username</label>
@@ -33,7 +38,7 @@
             </div>
 
             <div class="form-group">
-                <button type="submit" name="save" class="btn btn-success btn-lg btn-block">
+                <button type="submit" name="save" class="btn btn-lg btn-block" style="background: mediumaquamarine; color: white;">
                     Register now
                 </button>
             </div>
